91532dd67499c640cbfd5b69f2781889da85720a
newsun <jaiviopr@126.com>
2012-12-17 19:41:25 +0800

    Added SBTM module

---
+++ src/main/java/com/symbio/sbtm/client/SBTM.java
@@ -0,0 +1,13 @@
+package com.symbio.sbtm.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class SBTM implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
